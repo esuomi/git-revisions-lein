a423c98028bbf51180ded6a2a76d5113ab30ac1c
Esko Suomi <esko.suomi@solita.fi>
2022-06-09 14:56:07 +0300

    re-harmonize environment variable naming pattern between git-revisions projects

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject fi.polycode/lein-git-revisions "_"
-  :description "Automatically control Leiningen project version based on Git metadata."
+  :description "Automatically control Leiningen project version based on Git and system metadata."
   :url "https://github.com/esuomi/git-revisions-lein"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :scm {:name "git" :url "https://github.com/esuomi/git-revisions"}
+  :scm {:name "git" :url "https://github.com/esuomi/git-revisions-lein"}
 
   :eval-in-leiningen true
 
@@ -24,7 +24,7 @@
                                   [lambdaisland/kaocha-cloverage "1.0.75"]]}}
 
   :git-revisions {:format        :semver
-                  :adjust        [:env/lein_revisions_adjustments :minor]
+                  :adjust        [:env/LEIN_REVISIONS_ADJUSTMENT :minor]
                   :revision-file "resources/metadata.edn"}
 
   :aliases {"kaocha" ["run" "-m" "kaocha.runner"]})
